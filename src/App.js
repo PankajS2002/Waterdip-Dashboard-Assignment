7fac6186cc46b54f40d3b9d542e400f7d6954a1b
Pankaj <pankajsonake27@gmail.com>
2023-11-12 15:27:40 +0530

    added date range filter on data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import ApexCharts from 'react-apexcharts';
+import React, { useState, useEffect, useRef } from 'react';
 import Papa from 'papaparse';
 import Data from './data.csv';
+import ApexCharts from 'apexcharts';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [allData, setAllData] = useState([]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const chart1Ref = useRef(null);
+  const chart2Ref = useRef(null);
+  const chart3Ref = useRef(null);
+  const chart4Ref = useRef(null);
 
   useEffect(() => {
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
-];
     const fetchData = async () => {
       const response = await fetch(Data);
       const reader = response.body.getReader();
@@ -21,301 +28,310 @@ const App = () => {
         skipEmptyLines: true,
       }).data;
 
-      // Filter data for the specified date range (1 July 2015 to 3 July 2015)
-      const startDate = new Date('2015-07-01');
-      const endDate = new Date('2015-07-03');
-      console.log('Start date:', startDate);
-console.log('End date:', endDate);
-      const filteredData = parsedData.filter(item => {
-        const monthNumber = monthNames.indexOf(item.arrival_date_month);
-        const currentDate = new Date(item.arrival_date_year, monthNumber, parseInt(item.arrival_date_day_of_month));
-        
-        // Log the date values
-        console.log('Year:', item.arrival_date_year);
-        console.log('Month:', item.arrival_date_month);
-        console.log('Day:', item.arrival_date_day_of_month);
-        console.log('Parsed date:', currentDate);
-      
-        return currentDate >= startDate && currentDate <= endDate;
+      // Convert date strings to Date objects
+      const monthMapping = {
+        'January': '01',
+        'February': '02',
+        'March': '03',
+        'April': '04',
+        'May': '05',
+        'June': '06',
+        'July': '07',
+        'August': '08',
+        'September': '09',
+        'October': '10',
+        'November': '11',
+        'December': '12'
+      };
+      parsedData.forEach(item => {
+        const dateStr = `${item.arrival_date_year}-${monthMapping[item.arrival_date_month]}-${item.arrival_date_day_of_month}`;
+        item.date = new Date(dateStr);
       });
-      setData(filteredData);
+
+      setData(parsedData);
+      setAllData(parsedData);
     };
     fetchData();
-  }, []); // Empty dependency array to run the effect only once on mount
+  }, []);
 
+  const generateData = () => {
+    const startDate = new Date('2023-01-01');
+    const endDate = new Date('2023-01-31');
+    const dateArray = [];
+    const priceArray = [];
 
-  function dateIsValid(date) {
-    return !Number.isNaN(date.getTime());
-  }
-  // Calculate total number of visitors per day
-  const visitorsPerDay = {};
-  data.forEach(item => {
-    const currentDate = new Date(item.arrival_date_year, item.arrival_date_month - 1, item.arrival_date_day_of_month);
-    if (dateIsValid(currentDate)) {
-      const dateString = currentDate.toISOString().split('T')[0];
-      const totalVisitors = parseInt(item.adults) + parseInt(item.children) + parseInt(item.babies);
-      visitorsPerDay[dateString] = (visitorsPerDay[dateString] || 0) + totalVisitors;
-    } else {
-      console.warn('Invalid date:', item);
+    for (let date = startDate; date <= endDate; date.setDate(date.getDate() + 1)) {
+      dateArray.push(new Date(date));
+      priceArray.push(Math.random() * 1000000);
     }
-  });
-  
 
-  // Calculate total number of visitors per country
-  const visitorsPerCountry = {};
-  data.forEach(item => {
-    const country = item.country;
-    const totalVisitors = parseInt(item.adults) + parseInt(item.children) + parseInt(item.babies);
-    visitorsPerCountry[country] = (visitorsPerCountry[country] || 0) + totalVisitors;
-  });
+    const timestamps = dateArray.map(date => date.getTime());
 
-  // Calculate total number of adult visitors
-  const totalAdultVisitors = data.reduce((acc, item) => acc + parseInt(item.adults), 0);
+    const data = timestamps.map((timestamp, index) => [timestamp, priceArray[index]]);
 
-  // Calculate total number of children visitors
-  const totalChildrenVisitors = data.reduce((acc, item) => acc + parseInt(item.children), 0);
+    return data;
+  };
 
-  // Chart options
-  const timeSeriesOptions = {
+  const dates = generateData();
+ useEffect(() => {
+  const options = {
     series: [{
-      name: 'Number of Visitors',
-      data: Object.entries(visitorsPerDay).map(([date, visitors]) => ({ x: date, y: visitors })),
+      name: 'XYZ MOTORS',
+      data: dates,
     }],
-    options: {
-      chart: {
-        type: 'area',
-        stacked: false,
-        height: 350,
-        zoom: {
-          type: 'x',
-          enabled: true,
-          autoScaleYaxis: true,
-        },
-        toolbar: {
-          autoSelected: 'zoom',
-        },
+    chart: {
+      type: 'area',
+      stacked: false,
+      height: 350,
+      zoom: {
+        type: 'x',
+        enabled: true,
+        autoScaleYaxis: true,
       },
-      dataLabels: {
-        enabled: false,
+      toolbar: {
+        autoSelected: 'zoom',
       },
-      markers: {
-        size: 0,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    markers: {
+      size: 0,
+    },
+    title: {
+      text: 'Stock Price Movement',
+      align: 'left',
+    },
+    fill: {
+      type: 'gradient',
+      gradient: {
+        shadeIntensity: 1,
+        inverseColors: false,
+        opacityFrom: 0.5,
+        opacityTo: 0,
+        stops: [0, 90, 100],
       },
-      title: {
-        text: 'Number of Visitors per Day',
-        align: 'left',
-      },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shadeIntensity: 1,
-          inverseColors: false,
-          opacityFrom: 0.5,
-          opacityTo: 0,
-          stops: [0, 90, 100],
-        },
-      },
-      yaxis: {
-        labels: {
-          formatter: function (val) {
-            return (val).toFixed(0);
-          },
+    },
+    yaxis: {
+      labels: {
+        formatter: function (val) {
+          return (val).toFixed(0);
         },
       },
-      xaxis: {
-        type: 'datetime',
+    },
+    xaxis: {
+      type: 'datetime',
+    },
+    tooltip: {
+      shared: false,
+      y: {
+        formatter: function (val) {
+          return val.toFixed(0);
+        },
+      },
+    },
+  };
+
+  const options2 = {
+    series: [{
+      name: 'Inflation',
+      data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2]
+    }],
+    chart: {
+      height: 350,
+      type: 'bar',
+    },
+    plotOptions: {
+      bar: {
+        borderRadius: 10,
+        dataLabels: {
+          position: 'top',
+        },
+      }
+    },
+    dataLabels: {
+      enabled: true,
+      formatter: function (val) {
+        return val + "%";
+      },
+      offsetY: -20,
+      style: {
+        fontSize: '12px',
+        colors: ["#304758"]
+      }
+    },
+
+    xaxis: {
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      position: 'top',
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false
+      },
+      crosshairs: {
+        fill: {
+          type: 'gradient',
+          gradient: {
+            colorFrom: '#D8E3F0',
+            colorTo: '#BED1E6',
+            stops: [0, 100],
+            opacityFrom: 0.4,
+            opacityTo: 0.5,
+          }
+        }
       },
       tooltip: {
-        shared: false,
-        y: {
-          formatter: function (val) {
-            return val.toFixed(0);
-          },
-        },
-      },
-    },
-  };
-
-  const columnChartOptions = {
-    series: [{
-      name: 'Number of Visitors',
-      data: Object.entries(visitorsPerCountry).map(([country, visitors]) => ({ x: country, y: visitors })),
-    }],
-    options: {
-      chart: {
-        type: 'bar',
-        height: 350,
-      },
-      plotOptions: {
-        bar: {
-          borderRadius: 10,
-          dataLabels: {
-            position: 'top',
-          },
-        },
-      },
-      dataLabels: {
         enabled: true,
+      }
+    },
+    yaxis: {
+      axisBorder: {
+        show: false
+      },
+      axisTicks: {
+        show: false,
+      },
+      labels: {
+        show: false,
         formatter: function (val) {
-          return val;
-        },
-        offsetY: -20,
-        style: {
-          fontSize: '12px',
-          colors: ["#304758"],
-        },
-      },
-      xaxis: {
-        categories: Object.keys(visitorsPerCountry),
-        position: 'top',
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-        crosshairs: {
-          fill: {
-            type: 'gradient',
-            gradient: {
-              colorFrom: '#D8E3F0',
-              colorTo: '#BED1E6',
-              stops: [0, 100],
-              opacityFrom: 0.4,
-              opacityTo: 0.5,
-            },
-          },
-        },
-        tooltip: {
-          enabled: true,
-        },
-      },
-      yaxis: {
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
-        labels: {
-          show: false,
-          formatter: function (val) {
-            return val;
-          },
-        },
-      },
-      title: {
-        text: 'Number of Visitors per Country',
-        floating: true,
-        offsetY: 330,
-        align: 'center',
-        style: {
-          color: '#444',
-        },
-      },
+          return val + "%";
+        }
+      }
+
     },
+    title: {
+      text: 'Monthly Inflation in Argentina, 2002',
+      floating: true,
+      offsetY: 330,
+      align: 'center',
+      style: {
+        color: '#444'
+      }
+    }
   };
 
-  const sparklineOptions1 = {
+  const options3 = {
     series: [{
-      data: [totalAdultVisitors],
+      data: [23, 11, 54, 36, 26, 32, 2, 52,10,15]
     }],
-    options: {
-      chart: {
-        type: 'area',
-        height: 160,
-        sparkline: {
-          enabled: true,
-        },
-      },
-      stroke: {
-        curve: 'straight',
-      },
-      fill: {
-        opacity: 0.3,
-      },
-      yaxis: {
-        min: 0,
-      },
-      colors: ['#DCE6EC'],
-      title: {
-        text: `${totalAdultVisitors}`,
-        offsetX: 0,
-        style: {
-          fontSize: '24px',
-        },
-      },
-      subtitle: {
-        text: 'Total Adult Visitors',
-        offsetX: 0,
-        style: {
-          fontSize: '14px',
-        },
+    chart: {
+      type: 'area',
+      height: 160,
+      sparkline: {
+        enabled: true
       },
     },
+    stroke: {
+      curve: 'straight'
+    },
+    fill: {
+      opacity: 0.3,
+    },
+    yaxis: {
+      min: 0
+    },
+    colors: ['#DCE6EC'],
+    title: {
+      text: '$424,652',
+      offsetX: 0,
+      style: {
+        fontSize: '24px',
+      }
+    },
+    subtitle: {
+      text: 'Sales',
+      offsetX: 0,
+      style: {
+        fontSize: '14px',
+      }
+    }
   };
 
-  const sparklineOptions2 = {
+  const options4 = {
     series: [{
-      data: [totalChildrenVisitors],
+      data: [23, 11, 54, 36, 26, 21, 2, 10, 33, 20, 111]
     }],
-    options: {
-      chart: {
-        type: 'area',
-        height: 160,
-        sparkline: {
-          enabled: true,
-        },
-      },
-      stroke: {
-        curve: 'straight',
-      },
-      fill: {
-        opacity: 0.3,
-      },
-      yaxis: {
-        min: 0,
-      },
-      colors: ['#DCE6EC'],
-      title: {
-        text: `${totalChildrenVisitors}`,
-        offsetX: 0,
-        style: {
-          fontSize: '24px',
-        },
-      },
-      subtitle: {
-        text: 'Total Children Visitors',
-        offsetX: 0,
-        style: {
-          fontSize: '14px',
-        },
+    chart: {
+      type: 'area',
+      height: 160,
+      sparkline: {
+        enabled: true
       },
     },
+    stroke: {
+      curve: 'straight'
+    },
+    fill: {
+      opacity: 0.3,
+    },
+    yaxis: {
+      min: 0
+    },
+    colors: ['#DCE6EC'],
+    title: {
+      text: '$424,652',
+      offsetX: 0,
+      style: {
+        fontSize: '24px',
+      }
+    },
+    subtitle: {
+      text: 'Sales',
+      offsetX: 0,
+      style: {
+        fontSize: '14px',
+      }
+    }
   };
-  console.log('visitorsPerDay:', visitorsPerDay);
-  console.log('visitorsPerCountry:', visitorsPerCountry);
-  console.log('totalAdultVisitors:', totalAdultVisitors);
-  console.log('totalChildrenVisitors:', totalChildrenVisitors);
+
+  
+  const chart1 = new ApexCharts(chart1Ref.current, options);
+  chart1.render();
+
+  const chart2 = new ApexCharts(chart2Ref.current, options2);
+  chart2.render();
+
+  const chart3 = new ApexCharts(chart3Ref.current, options3);
+  chart3.render();
+
+  const chart4 = new ApexCharts(chart4Ref.current, options4);
+  chart4.render();
+
+  return () => {
+    chart1.destroy();
+    chart2.destroy();
+    chart3.destroy();
+    chart4.destroy();
+  };
+}, []);
+
+const handleSelect = (date) => {
+  let filteredData = allData.filter(item => item.date >= date.selection.startDate && item.date <= date.selection.endDate);
+  setStartDate(date.selection.startDate);
+  setEndDate(date.selection.endDate);
+  setData(filteredData);
+
+}
+const selectionRange = {
+  startDate: startDate,
+  endDate: endDate,
+  key: 'selection',
+}
   return (
     <div>
+  
       <h2>Charts</h2>
+      <DateRangePicker
+        ranges={[selectionRange]}
+        onChange={handleSelect}
+      />
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
-        <div style={chartContainerStyle}>
-          <ApexCharts options={timeSeriesOptions.options} series={timeSeriesOptions.series} type="area" height={350} />
-        </div>
-
-        <div style={chartContainerStyle}>
-          <ApexCharts options={columnChartOptions.options} series={columnChartOptions.series} type="bar" height={350} />
-        </div>
-
-        <div style={chartContainerStyle}>
-          <ApexCharts options={sparklineOptions1.options} series={sparklineOptions1.series} type="area" height={160} />
-        </div>
-
-        <div style={chartContainerStyle}>
-          <ApexCharts options={sparklineOptions2.options} series={sparklineOptions2.series} type="area" height={160} />
-        </div>
+      <div ref={chart1Ref} style={chartContainerStyle}></div>
+      <div ref={chart2Ref} style={chartContainerStyle}></div>
+      <div ref={chart3Ref} style={chartContainerStyle}></div>
+      <div ref={chart4Ref} style={chartContainerStyle}></div>
       </div>
     </div>
   );
